fec622d9bc493610f6e2ed5705d188c5e9209404
johannaschuster <josmat26.js@gmail.com>
2023-04-24 22:24:23 +0200

    final changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { CameraProjections, IfcViewerAPI } from 'web-ifc-viewer';
-import { createSideMenuButton } from './utils/gui-creator';
+import { createSideMenuButton } from './utils/button';
 import {IFCSPACE, IFCOPENINGELEMENT, IFCFURNISHINGELEMENT, IFCWALL, IFCWINDOW, IFCCURTAINWALL, IFCMEMBER, IFCPLATE
 } from 'web-ifc';
 import {
@@ -18,82 +18,7 @@ const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(255, 255
 viewer.axes.setAxes();
 viewer.grid.setGrid();
 
-const stats = new Stats();
-stats.showPanel(2);
-document.body.append(stats.dom);
-stats.dom.style.right = '0px';
-stats.dom.style.left = 'auto';
-viewer.context.stats = stats;
-
-viewer.context.ifcCamera.cameraControls
-
 const manager = viewer.IFC.loader.ifcManager;
-
-async function getAllWallMeshes() {
- const wallsIDs = manager.getAllItemsOfType(0, IFCWALL, false);
- const meshes = [];
-  const customID = 'temp-gltf-subset';
-
-  for(const wallID of wallsIDs) {
-   const coordinates = [];
-   const expressIDs = [];
-   const newIndices = [];
-
-   const alreadySaved = new Map();
-
-   const subset = viewer.IFC.loader.ifcManager.createSubset({
-     ids: [wallID],
-     modelID,
-     removePrevious: true,
-     customID
-   });
-
-   const positionAttr = subset.geometry.attributes.position;
-   const expressIDAttr = subset.geometry.attributes.expressID;
-
-   const newGroups = subset.geometry.groups.filter((group) => group.count !== 0);
-   const newMaterials = [];
-   const prevMaterials = subset.material;
-   let newMaterialIndex = 0;
-   newGroups.forEach((group) => {
-     newMaterials.push(prevMaterials[group.materialIndex]);
-     group.materialIndex = newMaterialIndex++;
-   });
-
-   let newIndex = 0;
-   for (let i = 0; i < subset.geometry.index.count; i++) {
-     const index = subset.geometry.index.array[i];
-
-     if (!alreadySaved.has(index)) {
-       coordinates.push(positionAttr.array[3 * index]);
-       coordinates.push(positionAttr.array[3 * index + 1]);
-       coordinates.push(positionAttr.array[3 * index + 2]);
-
-       expressIDs.push(expressIDAttr.getX(index));
-       alreadySaved.set(index, newIndex++);
-     }
-
-     const saved = alreadySaved.get(index);
-     newIndices.push(saved);
-   }
-
-   const geometryToExport = new BufferGeometry();
-   const newVerticesAttr = new BufferAttribute(Float32Array.from(coordinates), 3);
-   const newExpressIDAttr = new BufferAttribute(Uint32Array.from(expressIDs), 1);
-
-   geometryToExport.setAttribute('position', newVerticesAttr);
-   geometryToExport.setAttribute('expressID', newExpressIDAttr);
-   geometryToExport.setIndex(newIndices);
-   geometryToExport.groups = newGroups;
-   geometryToExport.computeVertexNormals();
-
-   const mesh = new Mesh(geometryToExport, newMaterials);
-   meshes.push(mesh);
- }
-
-  viewer.IFC.loader.ifcManager.removeSubset(modelID, undefined, customID);
-  return meshes;
-}
 
 viewer.IFC.setWasmPath('files/');
 
@@ -112,31 +37,7 @@ const loadIfc = async (event) => {
   const selectedFile = event.target.files[0];
   if(!selectedFile) return;
 
-  const overlay = document.getElementById('loading-overlay');
-  const progressText = document.getElementById('loading-progress');
-
-  overlay.classList.remove('hidden');
-
-  viewer.IFC.loader.ifcManager.setOnProgress((event) => {
-    const percentage = Math.floor((event.loaded * 100) / event.total);
-  });
-
-  viewer.IFC.loader.ifcManager.parser.setupOptionalCategories({
-    [IFCSPACE]: false,
-    [IFCOPENINGELEMENT]: false
-  });
-
   model = await viewer.IFC.loadIfc(selectedFile, false);
-
-
-  if(first) first = false
-  else {
-    ClippingEdges.forceStyleUpdate = true;
-  }
-
-  await viewer.shadowDropper.renderShadow(model.modelID);
-
-  overlay.classList.add('hidden');
 
 };
 
@@ -144,37 +45,6 @@ const inputElement = document.createElement('input');
 inputElement.setAttribute('type', 'file');
 inputElement.classList.add('hidden');
 inputElement.addEventListener('change', loadIfc, false);
-
-const handleKeyDown = async (event) => {
-  if (event.code === 'Delete') {
-    viewer.clipper.deletePlane();
-    viewer.dimensions.delete();
-  }
-  if (event.code === 'Escape') {
-    viewer.IFC.selector.unHighlightIfcItems();
-  }
-  if (event.code === 'KeyC') {
-    viewer.context.ifcCamera.toggleProjection();
-  }
-  if (event.code === 'KeyD') {
-    viewer.IFC.removeIfcModel(0);
-  }
-};
-
-window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
-window.onkeydown = handleKeyDown;
-window.ondblclick = async () => {
-
-  if (viewer.clipper.active) {
-    viewer.clipper.createPlane();
-  } else {
-    const result = await viewer.IFC.selector.highlightIfcItem(true);
-    if (!result) return;
-    const { modelID, id } = result;
-    const props = await viewer.IFC.getProperties(modelID, id, true, false);
-    console.log(props);
-  }
-};
 
 const loadButton = createSideMenuButton('./resources/folder-icon.svg');
 loadButton.addEventListener('click', () => {
