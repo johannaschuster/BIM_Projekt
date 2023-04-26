ef32fb633271d417cbbe53f0b223620bfef42f61
johannaschuster <josmat26.js@gmail.com>
2023-04-26 14:49:35 +0200

    löschen button fertig

--- utils/button.js
+++ utils/button.js
@@ -1,8 +1,4 @@
 export function createUploadButton(){
-  /*const button = document.createElement('upload-button');
-  button.classList.add('upload-button');
-  button.innerHTML ="Modell hochladen";
-*/
 const button = document.createElement('upload-button');
 button.innerHTML = "Modell hochladen";
 button.type = "hochladen";
@@ -10,7 +6,3 @@ button.name = "upload";
 document.body.appendChild(button);
 return button;
 }
-// let btn = document.createElement("upload-button");
-//btn.innerHTML = "Modell hochladen";
-//document.body.appendChild(btn);
-//return button;
