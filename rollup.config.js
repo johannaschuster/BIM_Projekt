794e13672a44e18509a0117d093d1398d6adca12
johannaschuster <josmat26.js@gmail.com>
2023-05-08 16:16:56 +0200

    Fehlerbehebung

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-// rollup.config.js
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-export default {
-  input: "./src/index.js",
-  output: {
-    format: "iife",
-    file: "./build/bundle.js",
-  },
-  plugins: [nodeResolve(), commonjs()],
-=======
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
@@ -21,5 +9,4 @@ export default {
     inlineDynamicImports: true, 
   },
   plugins: [ nodeResolve(), commonjs() ]
->>>>>>> c1f6ba6c80c59398ba8ee27423af9536b2eda266
 };
